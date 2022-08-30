9bcd41319abe823f558562e37c847a0e182e2a71
karlinemelo <karlinemelo@gmail.com>
2022-08-29 21:13:47 -0400

    Resolucao dos tres primeiros exercicios de JavaScript

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+/**a)** Declare uma variável para armazenar um **nome**, sem atribuir um valor.*/
+let nome = prompt("Qual o seu nome?");
+console.log(nome);
+
+/**b)** Declare uma variável para armazenar uma **idade**, sem atribuir um valor.*/
+let idade = prompt("Qual a sua idade?");
+console.log(idade);
+
+/**c)** Imprima na tela o **tipo** dessas variáveis que acabou de criar, usando o comando `typeof`.*/
+console.log(typeof nome);
+console.log(typeof idade);
+console.log("Olá! Me chamo", nome, "tenho", idade, "anos.");
+
+/**d)** Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.*/
+// Esse tipo foi impresso para descobrir o tipo da variável
+
+/**e)** Depois, disso, atualize o código para **perguntar** ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.*/
